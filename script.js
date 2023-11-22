a6bf6d33525e863315a5bc7d1247b51647de4a71
kikangseok <rlrkdtjr123@naver.com>
2023-11-22 14:02:28 +0900

    Refactor: Student 클래스 도입 학생 관련 데이터 및 동작을 캡슐화하기 위한 Student 클래스 생성 학생 정보 관리를 위해 Student 클래스를 사용하는 방식으로 코드 리팩토링 Student 클래스를 통해 학생 관련 로직을 캡슐화하여 모듈성과 가독성 향상

--- script.js
+++ script.js
@@ -1,63 +1,109 @@
 "use strict";
 
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.programs = []; //학생이 참가한 프로그램들을 저장하는 array
+  }
+
+  // 학생이 참가한 프로그램을 추가하는 메소드
+  addProgram(programName) {
+    if (programName.trim() !== "") {
+      this.programs.push({
+        name: programName,
+        checked: false,
+        checkTime: null,
+      });
+    }
+  }
+  // 학생의 특정 프로그램의 출석을 표시하는 메소드
+  markProgramAttendance(programIndex) {
+    if (programIndex >= 0 && programIndex < this.programs.length) {
+      const program = this.programs[programIndex];
+      program.checked = true;
+      program.checkTime = new Date();
+    }
+  }
+  // 학생이 참가한 프로그램 목록에서 특정 프로그램 제거하는 메소드
+  removeProgram(programIndex) {
+    if (programIndex >= 0 && programIndex < this.programs.length) {
+      this.programs.splice(programIndex, 1);
+    }
+  }
+  // 학생의 이름과 참여한 프로그램 목록을 객체 형태로 반환하는 메소드
+  getAttendanceInfo() {
+    return {
+      name: this.name,
+      programs: this.programs,
+    };
+  }
+}
+
 // 학생 출석을 표시하는 함수
 function markAttendance() {
-  const studentName = document.getElementById("studentName").value;
+  const studentName = document.getElementById("studentName").value.trim();
 
   if (studentName === "") {
     alert("학생 이름을 입력해주세요!");
     return;
   }
 
-  // createStudentListItem 함수 사용
-  const li = createStudentListItem(studentName);
-  addProgramsToStudent(li);
+  // 새로운 Student object 생성
+  const student = new Student(studentName);
+  const li = createStudentListItem(student);
 
   document.getElementById("attendanceList").appendChild(li);
   document.getElementById("studentName").value = "";
 }
 
 // 학생 목록 항목을 생성하는 함수
-function createStudentListItem(studentName) {
+function createStudentListItem(student) {
   const li = document.createElement("li");
-  const text = document.createTextNode(studentName);
+  const text = document.createTextNode(student.name);
   li.appendChild(text);
+
+  addProgramsToStudent(li, student);
 
   return li;
 }
 
 // 학생에게 프로그램을 추가하는 함수
-function addProgramsToStudent(li) {
+function addProgramsToStudent(li, student) {
   const programInputs = document.getElementsByClassName("programInput");
-
+  let programidx = 0;
   for (let i = 0; i < programInputs.length; i++) {
-    const programName = programInputs[i].value;
+    const programName = programInputs[i].value.trim();
 
     if (programName !== "") {
+      // student object에 프로그램 추가
+      student.addProgram(programName);
       // createCheckbox 함수 사용
-      const checkbox = createCheckbox(li, programName, i);
+      const checkbox = createCheckbox(li, student, programidx);
       li.appendChild(checkbox);
+      programidx += 1;
     }
   }
 
   // addRemoveButton 함수 사용
-  addRemoveButton(li);
+  addRemoveButton(li, student);
 }
 
 // 체크박스를 생성하는 함수
-function createCheckbox(li, programName, index) {
+function createCheckbox(li, student, programIndex) {
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
-  checkbox.id = `attendance-${li.firstChild.textContent}-${index}`;
+  checkbox.id = `attendance-${li.firstChild.textContent}-${programIndex}`;
 
   checkbox.addEventListener("change", function () {
     // handleCheckboxChange 함수 사용
-    handleCheckboxChange(this, programName, li);
+    handleCheckboxChange(this, student, programIndex, li);
   });
 
   const label = document.createElement("label");
   label.htmlFor = checkbox.id;
-  label.appendChild(document.createTextNode(programName));
+  label.appendChild(
+    document.createTextNode(student.programs[programIndex].name)
+  );
 
   li.appendChild(label);
   li.appendChild(checkbox);
@@ -66,17 +112,18 @@ function createCheckbox(li, programName, index) {
 }
 
 // 체크박스 상태의 변화를 처리하는 함수
-function handleCheckboxChange(checkbox, programName, li) {
-  const timeLabel = li.querySelector(`#check-time${programName}`);
+function handleCheckboxChange(checkbox, student, programIndex, li) {
+  student.markProgramAttendance(programIndex);
+  const program = student.programs[programIndex];
 
   if (checkbox.checked) {
     // createTimeLabel 함수 사용
     const now = new Date();
-    const timeLabel = createTimeLabel(programName, now);
+    const timeLabel = createTimeLabel(program.name, now);
     li.appendChild(timeLabel);
   } else {
     // removeTimeLabel 함수 사용
-    removeTimeLabel(li, timeLabel);
+    removeTimeLabel(li, program.name);
   }
 }
 
@@ -91,24 +138,30 @@ function createTimeLabel(programName, time) {
 }
 
 // 시간 라벨을 제거하는 함수
-function removeTimeLabel(li, timeLabel) {
+function removeTimeLabel(li, programName) {
+  const timeLabel = li.querySelector(`#check-time${programName}`);
+
   if (timeLabel) {
     li.removeChild(timeLabel);
   }
 }
 
 // 제거 버튼을 추가하는 함수
-function addRemoveButton(li) {
+function addRemoveButton(li, student) {
   const removeButton = document.createElement("button");
   removeButton.textContent = "x";
   removeButton.className = "removeButton";
   removeButton.addEventListener("click", function () {
-    li.remove();
+    removeStudent(li, student);
   });
 
   li.appendChild(removeButton);
 }
 
+function removeStudent(li, student) {
+  const list = document.getElementById("attendanceList");
+  list.removeChild(li);
+}
 // 프로그램 입력 필드를 토글하는 함수
 function toggleProgramInputs() {
   const programInputs = document.getElementById("programInputs");
